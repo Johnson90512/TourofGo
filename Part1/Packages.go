b04b85e09f436cd104b1a6ec17c42dccda45a02d
johnson90512 <johnson90512@gmail.com>
2017-09-08 16:09:05 -0400

    Added a description of what the code does

--- Part1/Packages.go
+++ Part1/Packages.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 )
-
+//This program is using the packages with import paths "fmt" and "math/rand".
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
