b25028f2c3a49da4027a9d73171539ab5cc49fdc
johnson90512 <johnson90512@gmail.com>
2017-09-08 16:09:56 -0400

    Learning about imports and how they can be written

---
+++ Part1/Imports.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+//This code groups the imports into a parenthesized, "factored" import statement
+func main(){
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+}
