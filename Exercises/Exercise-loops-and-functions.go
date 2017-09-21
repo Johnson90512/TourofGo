428b0a7151ff9369e2654c962e9da76257376493
johnson90512 <johnson90512@gmail.com>
2017-09-21 17:00:37 -0400

    Testing the Loop

--- Exercises/Exercise-loops-and-functions.go
+++ Exercises/Exercise-loops-and-functions.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func Sqrt(x float64) float64 {
+func Newt(x float64) float64 {
+
+	a := 1.0
+	for i :=1; i < 15; i++ {
+		a = a - ((a*a) - x) / (2 * a)
+	}
+	return a
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Newt(256))
+	fmt.Println(math.Sqrt(256))
 }
