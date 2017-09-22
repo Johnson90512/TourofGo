db73311b505ef1057346bffe1070a43bccef4088
johnson90512 <johnson90512@gmail.com>
2017-09-22 00:15:39 -0400

    Added println for every loop iteration

--- Exercises/Exercise-loops-and-functions.go
+++ Exercises/Exercise-loops-and-functions.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	//"math"
 )
 
 func Newt(x float64) float64 {
 
 	a := 1.0
-	for i :=1; i < 15; i++ {
+	for i :=1; i < 10; i++ {
 		a = a - ((a*a) - x) / (2 * a)
+		fmt.Println(a)
 	}
 	return a
 }
 
 func main() {
-	fmt.Println(Newt(256))
-	fmt.Println(math.Sqrt(256))
+	fmt.Println(Newt(100))
 }
