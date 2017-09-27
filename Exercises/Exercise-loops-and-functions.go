5149d109963fb0bcda428770f9728b17516df102
johnson90512 <johnson90512@gmail.com>
2017-09-26 23:26:56 -0400

    Limiting loop by differentiation

--- Exercises/Exercise-loops-and-functions.go
+++ Exercises/Exercise-loops-and-functions.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
-	//"math"
+	"math"
 )
 
 func Newt(x float64) float64 {
-
-	a := 1.0
-	for i :=1; i < 10; i++ {
-		a = a - ((a*a) - x) / (2 * a)
-		fmt.Println(a)
+	a, b := x,0.0
+	iterations := 0
+	for {
+		iterations++
+		a, b = a - ((a * a) - x) / (2 * a), a
+		fmt.Println(a - b)
+		if a - b < 0 {
+			b = -(a - b)
+		}
+		if (a - b) < 1e-8 {
+			break
+		}
 	}
+	fmt.Println("Iterations: ", iterations)
 	return a
 }
 
 func main() {
-	fmt.Println(Newt(100))
+	fmt.Println(Newt(13))
+	fmt.Println("Expected: ", math.Sqrt(19283389217398127))
 }
